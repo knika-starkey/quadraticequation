cb066c1632b4bb16ddde03fbd4e912e900d079c2
Veronika <72261304+knika-starkey@users.noreply.github.com>
2021-09-10 16:40:52 +0300

    Add files via upload

---
+++ quadraticequation.js
@@ -0,0 +1,19 @@
+alert("ax^2+bx+c=0");
+let a = prompt("Enter a");
+let b = prompt("Enter b");
+let c = prompt("Enter c");
+let D = b*b-4*a*c;
+alert("D="+b+"^2-4"+a+c+"="+D);
+if (D>0)
+{
+    let x1 = (-b + Math.sqrt(D))/2*a;
+    let x2 = (-b - Math.sqrt(D))/2*a;
+    alert("x1 = "+x1+"; x2 = "+x2);
+}
+else if (D=0)
+{
+let x = -b/(2*a);
+alert("x = "+x);
+}
+else
+alert("Expression has no roots");
