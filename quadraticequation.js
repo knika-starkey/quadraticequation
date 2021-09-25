574f590d1efab40e309ed72f4187bb7e6ec587fc
Veronika <72261304+knika-starkey@users.noreply.github.com>
2021-09-25 09:54:27 +0300

    Update quadraticequation.js

--- quadraticequation.js
+++ quadraticequation.js
@@ -4,22 +4,24 @@ let b = prompt("Enter b");
 let c = prompt("Enter c");
 if (isNaN(a) || isNaN(b) || isNaN(c)) alert("It must be numbers");
 else {
-  if (a == 0) {
-    let x = -c / b;
-    alert("x = " + x);
-  } else if (b == 0) {
-    let x1 = Math.sqrt(-c / a);
-    let x2 = Math.sqrt(c / a);
-  } else {
-    let D = b * b - 4 * a * c;
-    alert("D=" + b + "^2-4" + a + c + "=" + D);
-    if (D > 0) {
-      let x1 = (-b + Math.sqrt(D)) / (2 * a);
-      let x2 = (-b - Math.sqrt(D)) / (2 * a);
-      alert("x1 = " + x1 + "; x2 = " + x2);
-    } else if (D == 0) {
-      let x = -b / (2 * a);
-      alert("x = " + x);
-    } else alert("Expression has no roots");
-  }
+    if (a == 0) {
+        if (b != 0) {
+            let x = -c / b;
+            alert("x = " + x);
+        } else alert("Expression has no roots");
+    } else if (b == 0) {
+        let x1 = Math.sqrt(-c / a);
+        let x2 = Math.sqrt(c / a);
+    } else {
+        let D = b * b - 4 * a * c;
+        alert("D=" + b + "^2-4" + a + c + "=" + D);
+        if (D > 0) {
+            let x1 = (-b + Math.sqrt(D)) / (2 * a);
+            let x2 = (-b - Math.sqrt(D)) / (2 * a);
+            alert("x1 = " + x1 + "; x2 = " + x2);
+        } else if (D == 0) {
+            let x = -b / (2 * a);
+            alert("x = " + x);
+        } else alert("Expression has no roots");
+    }
 }
